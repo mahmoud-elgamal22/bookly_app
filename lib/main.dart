598db3974ea917ce6de1a17fa1c7dbb8f5a215e1
mahmoud-elgamal22 <melgamel@std.mans.edu.eg>
2024-10-15 14:40:39 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Bookly App'),
+        ),
+        body: const Center(
+          child: Text('Welcome to Bookly!'),
+        ),
+      ),
+    );
+  }
+}
