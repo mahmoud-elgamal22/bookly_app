b099f52ba31d6b1f87de951faaa9de895b33db6d
mahmoud-elgamal22 <melgamel@std.mans.edu.eg>
2024-10-16 03:05:45 +0300

    add custom card view to home

--- lib/Features/home/widgets/custom_appbar.dart
+++ lib/Features/home/widgets/custom_appbar.dart
@@ -8,7 +8,7 @@ class CustomAppBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(
-        horizontal: 32,
+        horizontal: 28,
         vertical: 70,
       ),
       child: Row(
--- lib/Features/home/widgets/home_view_body.dart
+++ lib/Features/home/widgets/home_view_body.dart
@@ -9,7 +9,34 @@ class HomeViewBody extends StatelessWidget {
     return const Column(
       children: <Widget>[
         CustomAppBar(),
+        CustomCardView(),
       ],
+    );
+  }
+}
+
+class CustomCardView extends StatelessWidget {
+  const CustomCardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.3,
+      child: AspectRatio(
+        aspectRatio: 2.8 / 4,
+        child: Container(
+          decoration: BoxDecoration(
+            image: const DecorationImage(
+              image: AssetImage(
+                'assets/images/test_image.png',
+              ),
+              fit: BoxFit.fill,
+            ),
+            color: Colors.red,
+            borderRadius: BorderRadius.circular(16),
+          ),
+        ),
+      ),
     );
   }
 }
